00eb046a492e35e7089b23f4eeba54e9f830d2ad
Chris Usick <christopher.usick@gmail.com>
2016-03-11 11:10:18 -0600

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'thom@chef.io'
 license 'apache2'
 description 'Installs/Configures kubernetes'
 long_description 'Installs/Configures kubernetes'
-version '0.1.0'
+version '0.1.1'
 
 depends "docker"
 depends "etcd"
